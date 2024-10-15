40be9799efc2cc39f1c89b9cf617e8e4bf92d751
26tatsuya <26tatsu7315@gmail.com>
2024-10-16 03:04:50 +0900

    ぺーさんのYAMLを参考にFastAPIの記述修正。フロントでのテストは未実施。

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, HTTPException
+from pydantic import BaseModel
+from typing import Optional
 
-app = FastAPI()
+app = FastAPI(
+    title="POSアプリAPI",
+    description="POSアプリのAPI仕様です。",
+    version="0.0.1",
+)
 
+class Product(BaseModel):
+    code: int
+    name: str
+    price: int
 
 @app.get("/")
 def read_root():
@@ -11,3 +21,10 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/get-products", summary="商品情報取得API")
+def get_products(code: int = Query(..., description="バーコードから読み取ったコード")):
+    if code == 12345:
+        return {"code": code, "name": "コーラ", "price": 120}
+    else:
+        raise HTTPException(status_code=404, detail="商品が見つかりませんでした")
