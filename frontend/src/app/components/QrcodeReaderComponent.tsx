940667450af3e04b6aceb607dba8832e5838c32a
26tatsuya <26tatsu7315@gmail.com>
2024-10-22 23:24:04 +0900

    商品情報取得APIへの読み取り後→Getの構成記載

--- frontend/src/app/components/QrcodeReaderComponent.tsx
+++ frontend/src/app/components/QrcodeReaderComponent.tsx
@@ -3,13 +3,19 @@
 'use client';
 import { useEffect, useState } from 'react';
 import QrcodeReader from './QrcodeReader';
- 
-export default function QrcodeReaderComponent() {
+
+interface QrcodeReaderComponentProps {
+  onScan: (result: string) => void;
+}
+
+export default function QrcodeReaderComponent({ onScan }: QrcodeReaderComponentProps) {
   const [scannedTime, setScannedTime] = useState(new Date());
   const [scannedResult, setScannedResult] = useState('');
- 
-  useEffect(() => {}, [scannedTime, scannedResult]);
- 
+
+  useEffect(() => {
+    onScan(scannedResult);
+  }, [scannedResult, onScan]);
+
   // QRコードを読み取った時の実行する関数
   const onNewScanResult = (result: any) => {
     console.log('QRコードスキャン結果');
@@ -17,18 +23,19 @@ export default function QrcodeReaderComponent() {
     setScannedTime(new Date());
     setScannedResult(result);
   };
+
   return (
     <>
-      <div>
-        <h2>スキャン日時：{scannedTime.toLocaleDateString()}</h2>
-        <h2>スキャン結果：{scannedResult}</h2>
-      </div>
       <QrcodeReader
         onScanSuccess={onNewScanResult}
         onScanFailure={(error: any) => {
           // console.log('Qr scan error');
         }}
       />
+      <div>
+        <h2 className='mt-6'>スキャン日時：{scannedTime.toLocaleDateString()}</h2>
+        <h2>スキャン結果：{scannedResult}</h2>
+      </div>
     </>
   );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,43 @@
-"use client";
+'use client';
 
-import React from "react";
-import QrcodeReader from "./components/QrcodeReader";
-import QrcodeReaderComponent from "./components/QrcodeReaderComponent";
+import React, { useState } from 'react';
+import QrcodeReaderComponent from './components/QrcodeReaderComponent';
 
-  export default function Home() {
+interface Product {
+  code: number;
+  name: string;
+  price: number;
+}
+
+export default function Home() {
+  const [scannedResult, setScannedResult] = useState('');
+  const [product, setProduct] = useState<Product | null>(null);
+
+  const handleCheck = async () => {
+    try {
+      const res = await fetch(`http://127.0.0.1:8000/get-products?code=${scannedResult}`);
+      if (!res.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data: Product = await res.json();
+      setProduct(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
-    <main>
-      <div className="text-2xl font-bold text-blue-600">POSアプリ</div>
-      <div>
-        <QrcodeReaderComponent />
-      </div>
-    </main>
+    <div className="flex flex-col">
+      <p className="text-2xl font-bold text-blue-600">POSアプリ</p>
+      <button className="btn btn-primary mt-4 w-48" onClick={handleCheck}>check</button>
+      {product && (
+        <div>
+          <p>コード: {product.code}</p>
+          <p>名前: {product.name}</p>
+          <p>価格: {product.price}円</p>
+        </div>
+      )}
+      <QrcodeReaderComponent onScan={setScannedResult} />
+    </div>
   );
 }
